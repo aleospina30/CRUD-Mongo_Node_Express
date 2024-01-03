5393a1d745e1df3d89cfb25baff6589c9a83205b
alejospina30 <alejospina08@gmail.com>
2024-01-03 14:18:33 -0500

    Configuración de Express-Handlebars

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import express from "express";
+import {engine} from 'express-handlebars';
 import indexRoutes from "./routes/index.routes.js";
+import path from 'path'
 
 const app = express();
 
+
+app.set('views', path.join(__dirname, 'views')); 
+
+//Express-Handlebars
+app.engine(".hbs", engine({
+    layoutDir: path.join(app.get("views"),"layouts"),
+    partialsDir: path.join(app.get("views"),"partials"),
+    defaultLayout: "main",
+    extname: ".hbs"
+}))
+app.set('view engine', '.hbs');
+
+//Routes
 app.use(indexRoutes);
 
 
