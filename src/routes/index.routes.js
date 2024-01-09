e11c3300306b8e37cd158fd711bdae720850ab8d
alejospina30 <alejospina08@gmail.com>
2024-01-09 12:02:40 -0500

    Se agregan las funciones en la carpeta controllers, además de esto se modifican las rutas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,61 +1,26 @@
 import { Router } from "express";
-import Task from "../models/Task";
+import {
+  createTask,
+  deleteTask,
+  editTask,
+  renderTaksEdit,
+  renderTasks,
+  taskToggleDone,
+} from "../controllers/task.controllers";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  //Se le agrega el .lena() para que devuelva objetos de js y no de MongoDB
-  const tasks = await Task.find().lean();
+router.get("/", renderTasks);
 
-  res.render("index", { tasks: tasks });
-});
+router.post("/tasks/add", createTask);
 
-router.post("/tasks/add", async (req, res) => {
-  try {
-    const task = Task(req.body);
+router.get("/tasks/:id/toggleDone", taskToggleDone);
 
-    await task.save();
+router.get("/tasks/:id/edit", renderTaksEdit);
 
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.post("/tasks/:id/edit", editTask);
 
-router.get("/about", (req, res) => {
-  res.render("about");
-});
+router.get("/tasks/:id/delete", deleteTask);
 
-router.get("/edit/:id", async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id).lean();
-    res.render("edit", { task });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-router.post("/edit/:id", async (req, res) => {
-  const { id } = req.params;
-
-  await Task.findByIdAndUpdate(id, req.body);
-  res.redirect("/");
-});
-
-router.get("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-
-  await Task.findByIdAndDelete(id);
-  res.redirect("/");
-});
-
-router.get("/toggleDone/:id", async (req, res) => {
-    const { id } = req.params;
-    const task = await Task.findById(id);
-
-    task.done = !task.done;
-    await task.save();
-    res.redirect("/");
-  });
 
 export default router;
